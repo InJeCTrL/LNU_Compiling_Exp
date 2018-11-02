7087878f32c3fdb231c22c969113c03dfa2fbf25
InJeCTrL <dap1933@hotmail.com>
2018-11-02 16:41:57 +0800

    关键字识别

--- KeywordSort/KeywordSort/t.c
+++ KeywordSort/KeywordSort/t.c
@@ -8,23 +8,42 @@ int main(void)
 							 "short","int","long","float","double","char","struct","union",
 							 "enum","typedef","const","unsigned","signed","extern","register",
 							 "static","volatile","void","goto","continue","default","sizeof","case"};
-	char *ptr = NULL;
+	char *ptr = NULL;//游标
+	char *fast = NULL;//超前指针
+	char t;//临时保存截断符
 	int KeyNum[32] = {0};//关键字出现次数
 	int i;
+
 	gets(str);//输入字符串
-	
-	for (i=0;i<32;i++)
-	{//检查所有关键字是否出现
-		ptr = str;//每次检查从开头开始
-		while (ptr = strstr(ptr,Keywords[i]))
-		{
-			KeyNum[i]++;//若找到关键字则关键字数量增加
-			ptr++;//跳过识别点
+	ptr = str;
+	while (*ptr)
+	{
+		while (*ptr && *ptr == ' ')
+			ptr++;//跳过空格
+		fast = ptr;//超前指针赋值
+		while (*fast)
+		{//超前指针后转
+			if (!*fast || *fast == ' ')
+				break;
+			fast++;
 		}
+		t = *fast;//保存截断符
+		*fast = 0;
+		for (i=0;i<32;i++)
+		{//检查所有关键字是否出现
+			if (strcmp(ptr,Keywords[i]) == 0)
+			{
+				KeyNum[i]++;//若找到关键字则关键字数量增加
+			}
+		}
+		*fast = t;//恢复截断符
+		ptr = fast;
 	}
+
 	for (i=0;i<32;i++)
 	{
-		printf("Number of keyword \" %s \" is %d\n",Keywords[i],KeyNum[i]);
+		if (KeyNum[i])
+			printf("Number of keyword \" %s \" is %d\n",Keywords[i],KeyNum[i]);
 	}
 
 	system("pause");
